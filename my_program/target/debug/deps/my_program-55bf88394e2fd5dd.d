7fd1552632147fd630c0ee379fce699521416bd0
DerekACO <derekcortez149@gmail.com>
2024-07-15 21:33:41 +0000

    hello

---
+++ my_program/target/debug/deps/my_program-55bf88394e2fd5dd.d
@@ -0,0 +1,5 @@
+/workspaces/rustc/my_program/target/debug/deps/my_program-55bf88394e2fd5dd: src/main.rs
+
+/workspaces/rustc/my_program/target/debug/deps/my_program-55bf88394e2fd5dd.d: src/main.rs
+
+src/main.rs:
---
+++ my_program/target/debug/my_program.d
@@ -0,0 +1,1 @@
+/workspaces/rustc/my_program/target/debug/my_program: /workspaces/rustc/my_program/src/main.rs
